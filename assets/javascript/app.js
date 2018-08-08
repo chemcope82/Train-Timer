3808c93e12e8ba6f82959af92205b7bf0a1159bd
Chemicalz82 <chemcope82@yahoo.com>
2018-08-08 18:29:36 -0500

    html and css done

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAO5z7sDO7rScf-5GhuDNNIsp7Rw0pVNPg",
+    authDomain: "train-timer-9b525.firebaseapp.com",
+    databaseURL: "https://train-timer-9b525.firebaseio.com",
+    projectId: "train-timer-9b525",
+    storageBucket: "",
+    messagingSenderId: "656113996506"
+  };
+  firebase.initializeApp(config);
+
