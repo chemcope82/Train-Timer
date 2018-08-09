d9125103807e1ea68eca604ad603b0f4d625fbf8
Chemicalz82 <chemcope82@yahoo.com>
2018-08-09 07:14:22 -0500

    completed the project - everything appears to be functioning correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,68 @@ var config = {
   };
   firebase.initializeApp(config);
 
+var db = firebase.database();
+
+$("#submitTrain").on("click", function(event){
+
+    event.preventDefault();
+
+    var trainName = $("#trainName").val();
+    var trainDestination = $("#trainDestination").val();
+    var firstTrain = $("#firstTrain").val();
+    var trainFrequency = $("#trainFrequency").val();
+
+    if (trainName !== "" && trainDestination!== "" && firstTrain !== "" && trainFrequency !== "") {
+        db.ref().push({
+            trainName: trainName,
+            trainDestination: trainDestination,
+            firstTrain: firstTrain,
+            trainFrequency: trainFrequency,
+        });
+
+        $("#trainName").val("");
+        $("#trainDestination").val("");
+        $("#firstTrain").val("");
+        $("#trainFrequency").val("");
+
+    } else {
+        alert("All fileds must be completed");
+    }
+})
+
+db.ref().on("child_added", function(snap){
+    //creating html elements to place text into
+    var newRow = $("<div class='row'>");
+    var newTrainName = $("<div class='col-md-3'>");
+    var newTrainDestination = $("<div class='col-md-3'>");
+    var newTrainFrequency = $("<div class='col-md-2'>");
+    var newNextArrival = $("<div class='col-md-2'>");
+    var newMinutesAway = $("<div class='col-md-2'>");
+    //creating variables to calculate time until next train
+    var firstTrain = snap.val().firstTrain;
+    var trainFrequency = parseInt(snap.val().trainFrequency);
+    var firstTrainConverted = moment(firstTrain, "HH:mm");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+    var timeRemainder = diffTime % trainFrequency
+    var minutesTilNextTrain;
+    var nextTrain;
+
+    if (moment().isAfter(firstTrainConverted)) {
+        minutesTilNextTrain = trainFrequency - timeRemainder;
+        nextTrain = moment().add(minutesTilNextTrain, "minutes").format("HH:mm");
+    } else {
+        minutesTilNextTrain = firstTrainConverted.diff(moment(), "minutes");
+        nextTrain = firstTrainConverted.format("HH:mm");
+    }
+
+
+    newTrainName.text(snap.val().trainName);
+    newTrainDestination.text(snap.val().trainDestination);
+    newTrainFrequency.text(snap.val().trainFrequency);
+    newNextArrival.text(nextTrain);
+    newMinutesAway.text(minutesTilNextTrain);
+
+    newRow.append(newTrainName, newTrainDestination, newTrainFrequency, newNextArrival, newMinutesAway);
+    $("#trainList").append(newRow);
+})
